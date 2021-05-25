9fb36a770f49ef75afeb196819c91a568fc35c57
Shubham Parihar <shubhamparihar391@gmail.com>
2021-05-25 18:56:39 +0530

    test: add code for testing tspotify

---
+++ tspotify-test/index.js
@@ -0,0 +1,22 @@
+import { Client } from 'tspotify';
+
+const client = new Client();
+
+client.on('ready', () => {
+    setInterval(async () => {
+        let searchedArtists = await client.artists.search({
+            query: 'Clairo',
+            limit: 1,
+            market: 'IN'
+        });
+        let artist = searchedArtists.items.first();
+        console.log(artist);
+    }, 300000);
+});
+
+const clientID = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+client.login({
+    clientID,
+    clientSecret
+});
